a0d6e06e1c6cdefe8a6d06f6b91709c07e796ffb
esu014 <esopenaurbano@gmail.com>
2025-01-19 17:42:47 +0100

    borrado de proyecto

--- 4º Rama Tecnologías de la Información/DCU/Entrega 3/proyecto/js/medicines-script.js
+++
@@ -1,6 +0,0 @@
-const botones = document.querySelectorAll('.btnDetalles');
-botones.forEach(boton => {
-    boton.addEventListener('click', () => {
-        window.location.href = './detalles-medicamento/index.html';
-    });
-});
